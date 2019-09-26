eb9c22ead850001bbf4f4fe89154d10b39f64bfe
TeraMeesawan <meesawantera@gmail.com>
2019-09-26 16:08:45 -0400

    Hell

--- src/array_functions.cpp
+++ src/array_functions.cpp
@@ -7,6 +7,13 @@
 
 //============================================================================
 //	TODO add necessary includes here
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include "constants.h"
+#include "array_functions.h"
 //============================================================================
 
 //============================================================================
@@ -21,3 +28,68 @@
 //TODO define all functions in header file
 
 //TODO look in utilities.h for useful functions, particularly strip_unwanted_chars!
+//zero out array that tracks words and their occurrences
+void clearArray(){
+
+}
+
+//how many unique words are in array
+int getArraySize(){
+	return 0;
+}
+
+//get data at a particular location
+std::string getArrayWordAt(int i);
+int getArrayWord_NumbOccur_At(int i){
+	return 0;
+}
+
+/*loop through whole file, one line at a time
+ * call processLine on each line
+ * returns false: myfstream is not open
+ *         true: otherwise*/
+bool processFile(std::fstream &myfstream){
+	return true;
+}
+
+/*take 1 line and extract all the tokens from it
+feed each token to processToken for recording*/
+void processLine(std::string &myString){
+
+}
+
+/*Keep track of how many times each token seen*/
+void processToken(std::string &token){
+
+}
+
+/*if you are debugging the file must be in the project parent directory
+  in this case Project2 with the .project and .cProject files*/
+bool openFile(std::fstream& myfile, const std::string& myFileName,
+	std::ios_base::openmode mode)
+{
+	return true;
+}
+
+/*iff myfile is open then close it*/
+void closeFile(std::fstream& myfile){
+
+}
+
+/* serializes all content in myEntryArray to file outputfilename
+ * returns  FAIL_FILE_DID_NOT_OPEN if cannot open outputfilename
+ * 			FAIL_NO_ARRAY_DATA if there are 0 entries in myEntryArray
+ * 			SUCCESS if all data is written and outputfilename closes OK
+ * */
+int writeArraytoFile(const std::string &outputfilename){
+	return 0;
+}
+
+/*
+ * Sort myEntryArray based on so enum value.
+ * You must provide a solution that handles alphabetic sorting (A-Z)
+ * The presence of the enum implies a switch statement based on its value
+ */
+void sortArray(constants::sortOrder so){
+
+}
